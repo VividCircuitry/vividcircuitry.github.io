08df0f1a957f464bd16c29463d3f22c30e706adf
VihaanChhabria <72771042+VihaanChhabria@users.noreply.github.com>
2024-05-10 20:39:07 -0400

    Update bluetooth.js

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -1,16 +1,25 @@
-function blueConnect(){
+// function blueConnect(){
+//     navigator.bluetooth.requestDevice({
+//         acceptAllDevices: true,
+//       })
+//       .then(device => { /* … */ })
+//       .catch(error => { console.error(error); });
+// }
+
+// function bluePush(){
+    
+// }
+
+
+// document.addEventListener("blueConnect", () => blueConnect())
+// document.addEventListener("bluePush", () => bluePush())
+
+button.addEventListener('pointermove', function(event) {
     navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
+        optionalServices: ['battery_service'] // Required to access service later.
       })
       .then(device => { /* … */ })
       .catch(error => { console.error(error); });
       
-}
-
-function bluePush(){
-    
-}
-
-
-document.addEventListener("blueConnect", () => blueConnect())
-document.addEventListener("bluePush", () => bluePush())
+});  
