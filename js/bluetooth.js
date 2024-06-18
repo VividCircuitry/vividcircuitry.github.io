4d6d5da9c50e34712a62c1c7bd1ed2e93892cec8
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-06-18 12:29:29 -0400

    added disconnect prevention

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -1,41 +1,53 @@
-// function blueConnect(){
-//     navigator.bluetooth.requestDevice({
-//         acceptAllDevices: true,
-//       })
-//       .then(device => { /* … */ })
-//       .catch(error => { console.error(error); });
-// }
+let bluetoothDevice = null;
 
-// function bluePush(){
-    
-// }
-
-
-// document.addEventListener("blueConnect", () => blueConnect())
-// document.addEventListener("bluePush", () => bluePush())
 document.getElementById('blueConnect')
     .addEventListener('click', function(event) {
-        navigator.bluetooth.requestDevice({
-            filters: [{
-              services: [0x180D]
-            }]
-          })
-          .then(device => device.gatt.connect())
-          .then(server => {
-            return server.getPrimaryService(0x180D);
-          })
-          .then(service => {
-            return service.getCharacteristic(0x2A37);
-          })
-          .then(characteristic => {
-            return characteristic.readValue();
-          })
-          .then(value => {
-            console.log(`Status is ${value.getUint8(0)}`);
-          })          
-          .catch(error => { console.error(error); });        
+        if (bluetoothDevice) {
+            getBlueStatus(bluetoothDevice, 0);
+        } else {
+            requestDevice();
+        }
     });
 
+function requestDevice() {
+    navigator.bluetooth.requestDevice({
+        filters: [{
+            services: [0x180D]
+        }]
+    })
+    .then(device => {
+        bluetoothDevice = device; // Store the device
+        return getBlueStatus(device, 0);
+    })
+    .catch(error => {
+        console.error('Error requesting Bluetooth device: ', error);
+    });
+}
 
-
-//
+function getBlueStatus(device, retries) {
+  device.gatt.connect()
+  .then(server => {
+      return server.getPrimaryService(0x180D);
+  })
+  .then(service => {
+      return service.getCharacteristic(0x2A37);
+  })
+  .then(characteristic => {
+      return characteristic.readValue();
+  })
+  .then(value => {
+      console.log(`Status is ${value.getUint8(0)}`);
+  })
+  .catch(error => {
+    console.error('DOMException occurred: ', error);
+    if (error instanceof DOMException) {
+      if (retries < 5) { // Set a maximum number of retries
+        getBlueStatus(device, retries + 1)
+      } else {
+        console.error('Max retries reached. Could not connect to Bluetooth device.');
+      }
+    } else {
+      console.error(error);
+    }
+  });
+}
