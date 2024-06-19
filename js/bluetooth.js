780e9b4d55fb7e6bede5b672d494e21e485be2d2
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-06-19 17:31:00 -0400

    removes semicolens

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -1,10 +1,10 @@
-bluetoothDevice = null;
+bluetoothDevice = null
 
 document.getElementById('blueConnect')
-  .addEventListener('click', () => blueConnect());
+  .addEventListener('click', () => blueConnect())
 
 //document.getElementById('bluePush')
-//  .addEventListener('click', () => bluePush());
+//  .addEventListener('click', () => bluePush())
 
 checkAndSend()
 
@@ -15,87 +15,86 @@ function blueConnect() {
     }]
   })
   .then(device => {
-    bluetoothDevice = device;
-    return forceConnect();
+    bluetoothDevice = device
+    return forceConnect()
   })
   .catch(error => {
-    console.error('Error requesting Bluetooth device: ', error);
-  });
+    console.error('Error requesting Bluetooth device: ', error)
+  })
 }
 
 async function checkAndSend() {
   while (true) {
-    await new Promise(r => setTimeout(r, 10000));
+    await new Promise(r => setTimeout(r, 10000))
     console.log("looping")
     if (bluetoothDevice) {
       statusValue = await getStatus()
       switch (statusValue) {
         case 1:
-          sendData(localStorage["jsonData"] || "");
-          break;
+          sendData(localStorage["jsonData"] || "")
+          break
       
         default:
-          break;
+          break
       }
     } else {
-      blueConnect();
+      blueConnect()
     }
   }
 }
 
 function sendData(stringData) {
-  console.log(stringData)
   if (bluetoothDevice.gatt.connected) {
     bluetoothDevice.gatt.getPrimaryService(0x180D)
       .then(service => {
-        return service.getCharacteristic(0x2A39);
+        return service.getCharacteristic(0x2A39)
       })
       .then(characteristic => {
         console.log('sending "' + stringData + '"')
-        return characteristic.writeValue(stringToArrayBuffer(stringData));
+        return characteristic.writeValue(encodeString(stringData))
       })
       .catch(error => {
-        console.error(error);
-      });
+        console.error(error)
+      })
   } else {
-    console.error('Device is not connected.');
+    console.error('Device is not connected.')
   }
 }
 
 function forceConnect() {
   bluetoothDevice.gatt.connect()
     .catch(error => {
-      console.error('DOMException occurred: ', error);
+      console.error('DOMException occurred: ', error)
       if (error instanceof DOMException) {
-        forceConnect();
+        forceConnect()
       } else {
-        console.error(error);
+        console.error(error)
       }
-    });
+    })
 }
 
 async function getStatus() {
-  let statusValue = 0;
+  let statusValue = 0
 
   try {
     if (bluetoothDevice.gatt.connected) {
-      const service = await bluetoothDevice.gatt.getPrimaryService(0x180D);
-      const characteristic = await service.getCharacteristic(0x2A37);
-      const value = await characteristic.readValue();
+      const service = await bluetoothDevice.gatt.getPrimaryService(0x180D)
+      const characteristic = await service.getCharacteristic(0x2A37)
+      const value = await characteristic.readValue()
       
-      statusValue = value.getUint8(0);
-      console.log(`Status is ${statusValue}`);
+      statusValue = value.getUint8(0)
+      console.log(`Status is ${statusValue}`)
     } else {
-      console.error('Device is not connected.');
+      console.error('Device is not connected.')
     }
   } catch (error) {
-    console.error('Error reading status:', error);
+    console.error('Error reading status:', error)
   }
 
-  return statusValue;
+  return statusValue
 }
 
-function stringToArrayBuffer(str) {
-  const encoder = new TextEncoder();
-  return encoder.encode(str);
+function encodeString(str) {
+  const encoder = new TextEncoder()
+  return encoder.encode(str)
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 jsonFull = ""
 localStorage["jsonData"] = jsonFull
 
-INPUT_TEXT = "text";
-INPUT_COUNTER = "counter";
-INPUT_CHECKBOX = "checkbox";
+INPUT_TEXT = "text"
+INPUT_COUNTER = "counter"
+INPUT_CHECKBOX = "checkbox"
 
 dataInputs = [
     [["matchNumber", INPUT_TEXT]],
@@ -23,9 +23,9 @@ dataInputs = [
     [["buddyClimb", INPUT_CHECKBOX]],
     [["brokeDown", INPUT_CHECKBOX]],
     [["comments", INPUT_TEXT]]
-];
+]
 
-document.getElementById("save").addEventListener("click", () => saveData());
+document.getElementById("save").addEventListener("click", () => saveData())
 
 
 const baseNames = [
@@ -45,42 +45,42 @@ const baseNames = [
 
 
 for (let index = 0; index < baseNames.length; index++) {
-    const baseName = baseNames[index];
+    const baseName = baseNames[index]
     createCounter(baseName)
 }
 
 function createCounter(baseName) {
-    const label = document.getElementById(baseName + "Lab");
+    const label = document.getElementById(baseName + "Lab")
 
-    const increaseButton = document.getElementById(baseName + "P");
-    const decreaseButton = document.getElementById(baseName + "N");
+    const increaseButton = document.getElementById(baseName + "P")
+    const decreaseButton = document.getElementById(baseName + "N")
 
-    increaseButton.addEventListener("click", () => increase(label));
-    decreaseButton.addEventListener("click", () => decrease(label));
+    increaseButton.addEventListener("click", () => increase(label))
+    decreaseButton.addEventListener("click", () => decrease(label))
 
     function increase(label) {
-        label.innerText = parseInt(label.innerText) + 1;
+        label.innerText = parseInt(label.innerText) + 1
     }
 
     function decrease(label) {
-        const value = parseInt(label.innerText);
+        const value = parseInt(label.innerText)
         if (value > 0) {
-            label.innerText = value - 1;
+            label.innerText = value - 1
         }
     }
 }
 
 function saveData() {
     for (let index = 0; index < dataInputs.length; index++) {
-        const element = dataInputs[index][0][0];
-        const elementType = dataInputs[index][0][1];
+        const element = dataInputs[index][0][0]
+        const elementType = dataInputs[index][0][1]
         
-        const input = document.getElementById(element);
-        data = "";
+        const input = document.getElementById(element)
+        data = ""
 
         switch (elementType) {
             case INPUT_COUNTER:
-                data = input.innerText;
+                data = input.innerText
 
                 if (((element == "matchNumber") || (element == "teamNumber") || (element == "initials")) && (data == "")){
                     alert(`Please fill out ${element} to save`)
@@ -88,18 +88,18 @@ function saveData() {
                 }
                 
                 data.innerText = 0
-                break;
+                break
 
             case INPUT_CHECKBOX:
-                data = input.checked;
+                data = input.checked
                 input.checked = false
-                break;
+                break
         
             default:
                 //INPUT_TEXT
-                data = input.value;
+                data = input.value
                 input.value = ""
-                break;
+                break
         }
 
         dataInputs[index].push(data)
@@ -107,25 +107,25 @@ function saveData() {
 
     jsonFile = "{\n"
     for (let index = 0; index < dataInputs.length; index++) {
-        const dataName = dataInputs[index][0][0];
-        const data = dataInputs[index][1];
+        const dataName = dataInputs[index][0][0]
+        const data = dataInputs[index][1]
         
         if (index == (dataInputs.length-1)){
-            jsonFile = jsonFile + '"' + dataName + '" : "' + data + '"\n';
+            jsonFile = jsonFile + '"' + dataName + '" : "' + data + '"\n'
         }else{
-            jsonFile = jsonFile + '"' + dataName + '" : "' + data + '",\n';
+            jsonFile = jsonFile + '"' + dataName + '" : "' + data + '",\n'
         }
     }
-    jsonFile = jsonFile + "}";
+    jsonFile = jsonFile + "}"
     jsonFull = jsonFull + jsonFile + ", "
     localStorage["jsonData"] = jsonFull
 
-    const file = new Blob([jsonFile], { type: 'text/plain' });
+    const file = new Blob([jsonFile], { type: 'text/plain' })
 
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(file);
-    link.download = "M" + dataInputs[0][1] + "_T" + dataInputs[1][1] + ".json";
+    const link = document.createElement("a")
+    link.href = URL.createObjectURL(file)
+    link.download = "M" + dataInputs[0][1] + "_T" + dataInputs[1][1] + ".json"
 
-    link.click();
-    URL.revokeObjectURL(link.href);
+    link.click()
+    URL.revokeObjectURL(link.href)
 }
