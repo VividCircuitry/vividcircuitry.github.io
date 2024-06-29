effffd3fb6efb4aa73608b301a484669a6cf6fb4
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-06-29 16:50:20 -0400

    changed chack name

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,33 @@
-const CACHE_NAME = 'v2';
-const CACHE_ASSETS = [
-  '/index.html',
-  '/misc/main.css',
-  '/js/script.js',
-  '/js/bluetooth.js'
-];
+const CACHE_NAME = "v1.1";
+const CACHE_ASSETS = ["/index.html", "/misc/main.css", "/js/script.js", "/js/bluetooth.js"];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caching files');
-        return cache.addAll(CACHE_ASSETS);
-      })
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => {
+                console.log("Caching files");
+                return cache.addAll(CACHE_ASSETS);
+            })
+            .then(() => self.skipWaiting())
+    );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Clearing old cache');
-            return caches.delete(cache);
-          }
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log("Clearing old cache");
+                        return caches.delete(cache);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener("fetch", (e) => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
