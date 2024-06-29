2c653834a899ea73c8ee67f3aaa5e9ef83695220
VihaanChhabria <chhabria.vihaan@gmail.com>
2024-06-29 09:58:10 -0400

    did formating

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -1,186 +1,176 @@
 let bluetoothDevice = null;
 
-document
-  .getElementById("blueConnect")
-  .addEventListener("click", () => blueConnect());
+document.getElementById("blueConnect").addEventListener("click", () => blueConnect());
 
 checkAndSend();
 
 setInterval(alertBluetooth, 1000);
 let blinkAlert = 1;
 
-document
-  .getElementById("clearMatchDetailsCache")
-  .addEventListener("click", () => checkMatches());
-document
-  .getElementById("getMatchesData")
-  .addEventListener("click", () => getMatches());
+document.getElementById("clearMatchDetailsCache").addEventListener("click", () => checkMatches());
+document.getElementById("getMatchesData").addEventListener("click", () => getMatches());
 
 function alertBluetooth() {
-  let alertButton = document.getElementById("alertBluetooth");
-  if (bluetoothDevice) {
-    alertButton.innerText = "You are connected to the Bluetooth network.";
-    alertButton.style.background = "#EE5622";
-  } else {
-    alertButton.style.background = blinkAlert == 1 ? "#FF0000" : "#0000FF";
-    blinkAlert = blinkAlert * -1;
-    alertButton.innerText = "Please connect to the Bluetooth network!";
-  }
+    let alertButton = document.getElementById("alertBluetooth");
+    if (bluetoothDevice) {
+        alertButton.innerText = "You are connected to the Bluetooth network.";
+        alertButton.style.background = "#EE5622";
+    } else {
+        alertButton.style.background = blinkAlert == 1 ? "#FF0000" : "#0000FF";
+        blinkAlert = blinkAlert * -1;
+        alertButton.innerText = "Please connect to the Bluetooth network!";
+    }
 }
 
 function blueConnect() {
-  navigator.bluetooth
-    .requestDevice({
-      filters: [{ services: [0x180d] }],
-    })
-    .then((device) => {
-      bluetoothDevice = device;
-      device.addEventListener("gattserverdisconnected", onDisconnected);
-      return forceConnect();
-    })
-    .catch((error) => {
-      console.error("Error requesting Bluetooth device: ", error);
-    });
+    navigator.bluetooth
+        .requestDevice({
+            filters: [{ services: [0x180d] }],
+        })
+        .then((device) => {
+            bluetoothDevice = device;
+            device.addEventListener("gattserverdisconnected", onDisconnected);
+            return forceConnect();
+        })
+        .catch((error) => {
+            console.error("Error requesting Bluetooth device: ", error);
+        });
 }
 
 function onDisconnected(event) {
-  bluetoothDevice = null;
+    bluetoothDevice = null;
 }
 
 async function checkAndSend() {
-  while (true) {
-    await new Promise((r) => setTimeout(r, 10000));
+    while (true) {
+        await new Promise((r) => setTimeout(r, 10000));
 
-    console.log("looping");
-    console.log(localStorage["jsonData"]);
-    if (bluetoothDevice && bluetoothDevice.gatt.connected) {
-      const statusValue = await getStatus();
-      if (
-        statusValue == parseInt(document.getElementById("scouterNum").value)
-      ) {
-        await cutAndSendData(localStorage["jsonData"] || "");
-      }
-    } else {
-      console.error("Device is not connected.");
+        console.log("looping");
+        console.log(localStorage["jsonData"]);
+        if (bluetoothDevice && bluetoothDevice.gatt.connected) {
+            const statusValue = await getStatus();
+            if (statusValue == parseInt(document.getElementById("scouterNum").value)) {
+                await cutAndSendData(localStorage["jsonData"] || "");
+            }
+        } else {
+            console.error("Device is not connected.");
+        }
     }
-  }
 }
 
 async function cutAndSendData(stringData) {
-  console.log('sending "' + stringData + '"');
+    console.log('sending "' + stringData + '"');
 
-  const fullEncodedStr = encodeString(stringData);
-  const chunkSize = 500;
+    const fullEncodedStr = encodeString(stringData);
+    const chunkSize = 500;
 
-  for (let i = 0; i < fullEncodedStr.length; i += chunkSize) {
-    const chunk = fullEncodedStr.subarray(i, i + chunkSize);
-    console.log(`Sending chunk of length: ${chunk.length}`);
-    await sendData(chunk);
-  }
+    for (let i = 0; i < fullEncodedStr.length; i += chunkSize) {
+        const chunk = fullEncodedStr.subarray(i, i + chunkSize);
+        console.log(`Sending chunk of length: ${chunk.length}`);
+        await sendData(chunk);
+    }
 }
 
 async function sendData(data) {
-  if (bluetoothDevice.gatt.connected) {
-    try {
-      const service = await bluetoothDevice.gatt.getPrimaryService(0x180d);
-      const characteristic = await service.getCharacteristic(0x2a39);
-      console.log(data);
-      await characteristic.writeValue(data);
-    } catch (error) {
-      console.error("Error writing data; trying again:", error);
-      await new Promise((r) => setTimeout(r, 2000));
-      await sendData(data);
+    if (bluetoothDevice.gatt.connected) {
+        try {
+            const service = await bluetoothDevice.gatt.getPrimaryService(0x180d);
+            const characteristic = await service.getCharacteristic(0x2a39);
+            console.log(data);
+            await characteristic.writeValue(data);
+        } catch (error) {
+            console.error("Error writing data; trying again:", error);
+            await new Promise((r) => setTimeout(r, 2000));
+            await sendData(data);
+        }
+    } else {
+        console.error("Device is not connected.");
     }
-  } else {
-    console.error("Device is not connected.");
-  }
 }
 
 function forceConnect() {
-  bluetoothDevice.gatt.connect().catch((error) => {
-    console.error("DOMException occurred: ", error);
-    if (error instanceof DOMException) {
-      forceConnect();
-    } else {
-      console.error(error);
-    }
-  });
+    bluetoothDevice.gatt.connect().catch((error) => {
+        console.error("DOMException occurred: ", error);
+        if (error instanceof DOMException) {
+            forceConnect();
+        } else {
+            console.error(error);
+        }
+    });
 }
 
 async function getStatus() {
-  let statusValue = 0;
+    let statusValue = 0;
 
-  try {
-    if (bluetoothDevice.gatt.connected) {
-      const service = await bluetoothDevice.gatt.getPrimaryService(0x180d);
-      const characteristic = await service.getCharacteristic(0x2a37);
-      const value = await characteristic.readValue();
+    try {
+        if (bluetoothDevice.gatt.connected) {
+            const service = await bluetoothDevice.gatt.getPrimaryService(0x180d);
+            const characteristic = await service.getCharacteristic(0x2a37);
+            const value = await characteristic.readValue();
 
-      statusValue = value.getUint8(0);
-      console.log(`Status is ${statusValue}`);
-    } else {
-      console.error("Device is not connected.");
+            statusValue = value.getUint8(0);
+            console.log(`Status is ${statusValue}`);
+        } else {
+            console.error("Device is not connected.");
+        }
+    } catch (error) {
+        bluetoothDevice = null;
+        console.error("Error reading status:", error);
     }
-  } catch (error) {
-    bluetoothDevice = null;
-    console.error("Error reading status:", error);
-  }
 
-  return statusValue;
+    return statusValue;
 }
 
 function decodeString(array) {
-  const decoder = new TextDecoder();
-  return decoder.decode(array);
+    const decoder = new TextDecoder();
+    return decoder.decode(array);
 }
 
 function encodeString(str) {
-  const encoder = new TextEncoder();
-  return encoder.encode(str);
+    const encoder = new TextEncoder();
+    return encoder.encode(str);
 }
 
 async function getMatches() {
-  matches = "";
-  if (bluetoothDevice && bluetoothDevice.gatt.connected) {
-    console.log("Bluetooth device is connected.");
-    dataLoop = true;
-    while (dataLoop) {
-      const matchesStatusService = await bluetoothDevice.gatt.getPrimaryService(
-        0x180d
-      );
-      const matchesStatusCharacteristic =
-        await matchesStatusService.getCharacteristic(0x2a91);
-      await matchesStatusCharacteristic.writeValue(encodeString("1"));
-      console.log("Wrote '1' to matchesStatusCharacteristic");
+    matches = "";
+    if (bluetoothDevice && bluetoothDevice.gatt.connected) {
+        console.log("Bluetooth device is connected.");
+        dataLoop = true;
+        while (dataLoop) {
+            const matchesStatusService = await bluetoothDevice.gatt.getPrimaryService(0x180d);
+            const matchesStatusCharacteristic = await matchesStatusService.getCharacteristic(
+                0x2a91
+            );
+            await matchesStatusCharacteristic.writeValue(encodeString("1"));
+            console.log("Wrote '1' to matchesStatusCharacteristic");
 
-      let matchesStatusValue = await matchesStatusCharacteristic.readValue();
-      matchesStatus = matchesStatusValue.getUint8(0);
-      console.log(`Initial matchesStatus: ${matchesStatus}`);
+            let matchesStatusValue = await matchesStatusCharacteristic.readValue();
+            matchesStatus = matchesStatusValue.getUint8(0);
+            console.log(`Initial matchesStatus: ${matchesStatus}`);
 
-      while (matchesStatus == 1) {
-        matchesStatusValue = await matchesStatusCharacteristic.readValue();
-        matchesStatus = matchesStatusValue.getUint8(0);
-        console.log(`Updated matchesStatus: ${matchesStatus}`);
-      }
+            while (matchesStatus == 1) {
+                matchesStatusValue = await matchesStatusCharacteristic.readValue();
+                matchesStatus = matchesStatusValue.getUint8(0);
+                console.log(`Updated matchesStatus: ${matchesStatus}`);
+            }
 
-      if (matchesStatus == 0) {
-        const matchesDataService = await bluetoothDevice.gatt.getPrimaryService(
-          0x180d
-        );
-        const matchesDataCharacteristic =
-          await matchesDataService.getCharacteristic(0x2a92);
-        matchesDataValue = await matchesDataCharacteristic.readValue()
-        matchesData = decodeString(matchesDataValue.buffer)
-        console.log(`matchesData: ${matchesData}`)
-        matches = matches + matchesData
-        console.log("Match data retrieved.");
-      } else {
-        dataLoop = false;
-      }
+            if (matchesStatus == 0) {
+                const matchesDataService = await bluetoothDevice.gatt.getPrimaryService(0x180d);
+                const matchesDataCharacteristic = await matchesDataService.getCharacteristic(
+                    0x2a92
+                );
+                matchesDataValue = await matchesDataCharacteristic.readValue();
+                matchesData = decodeString(matchesDataValue.buffer);
+                console.log(`matchesData: ${matchesData}`);
+                matches = matches + matchesData;
+                console.log("Match data retrieved.");
+            } else {
+                dataLoop = false;
+            }
+        }
+        console.log(matches);
+        localStorage["matches"] = matches;
+    } else {
+        console.error("Device is not connected.");
     }
-    console.log(matches);
-    localStorage["matches"] = matches;
-  } else {
-    console.error("Device is not connected.");
-  }
 }
--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-let jsonFull = ""
+let jsonFull = "";
 
-const INPUT_TEXT = "text"
-const INPUT_COUNTER = "counter"
-const INPUT_CHECKBOX = "checkbox"
+const INPUT_TEXT = "text";
+const INPUT_COUNTER = "counter";
+const INPUT_CHECKBOX = "checkbox";
 
 const dataInputs = [
     ["matchNumber", INPUT_TEXT],
@@ -22,10 +22,10 @@ const dataInputs = [
     ["climbed", INPUT_CHECKBOX],
     ["buddyClimb", INPUT_CHECKBOX],
     ["brokeDown", INPUT_CHECKBOX],
-    ["comments", INPUT_TEXT]
-]
+    ["comments", INPUT_TEXT],
+];
 
-document.getElementById("save").addEventListener("click", () => saveData())
+document.getElementById("save").addEventListener("click", () => saveData());
 
 const baseNames = [
     "autoAmpMade",
@@ -40,91 +40,97 @@ const baseNames = [
     "teleopSpeakerMissed",
     "trapMissed",
 
-    "notesFed"
-]
+    "notesFed",
+];
 
 for (let index = 0; index < baseNames.length; index++) {
-    const baseName = baseNames[index]
-    createCounter(baseName)
+    const baseName = baseNames[index];
+    createCounter(baseName);
 }
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/service-worker.js') // Ensure the correct path to your service worker
-        .then(reg => console.log('Service Worker registered'))
-        .catch(err => console.log(`Service Worker error: ${err}`));
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/service-worker.js") // Ensure the correct path to your service worker
+            .then((reg) => console.log("Service Worker registered"))
+            .catch((err) => console.log(`Service Worker error: ${err}`));
     });
-  }
-  
+}
+
 function createCounter(baseName) {
-    const label = document.getElementById(baseName + "Lab")
+    const label = document.getElementById(baseName + "Lab");
 
-    const increaseButton = document.getElementById(baseName + "P")
-    const decreaseButton = document.getElementById(baseName + "N")
+    const increaseButton = document.getElementById(baseName + "P");
+    const decreaseButton = document.getElementById(baseName + "N");
 
-    increaseButton.addEventListener("click", () => increase(label))
-    decreaseButton.addEventListener("click", () => decrease(label))
+    increaseButton.addEventListener("click", () => increase(label));
+    decreaseButton.addEventListener("click", () => decrease(label));
 
     function increase(label) {
-        label.innerText = parseInt(label.innerText) + 1
+        label.innerText = parseInt(label.innerText) + 1;
     }
 
     function decrease(label) {
-        const value = parseInt(label.innerText)
+        const value = parseInt(label.innerText);
         if (value > 0) {
-            label.innerText = value - 1
+            label.innerText = value - 1;
         }
     }
 }
 
 function saveData() {
-    let formData = {}
+    let formData = {};
 
     for (let index = 0; index < dataInputs.length; index++) {
-        const element = dataInputs[index][0]
-        const elementType = dataInputs[index][1]
-        
-        const input = document.getElementById(element)
-        let data = ""
+        const element = dataInputs[index][0];
+        const elementType = dataInputs[index][1];
+
+        const input = document.getElementById(element);
+        let data = "";
 
         switch (elementType) {
             case INPUT_COUNTER:
-                data = input.innerText                
-                input.innerText = 0
-                break
+                data = input.innerText;
+                input.innerText = 0;
+                break;
 
             case INPUT_CHECKBOX:
-                data = input.checked
-                input.checked = false
-                break
-        
+                data = input.checked;
+                input.checked = false;
+                break;
+
             default:
                 // INPUT_TEXT
-                data = input.value
-                if (((element === "matchNumber") || (element === "teamNumber") || (element === "initials") || (element === "scouterNum")) && (data === "")) {
-                    alert(`Please fill out ${element} to save`)
-                    return
+                data = input.value;
+                if (
+                    (element === "matchNumber" ||
+                        element === "teamNumber" ||
+                        element === "initials" ||
+                        element === "scouterNum") &&
+                    data === ""
+                ) {
+                    alert(`Please fill out ${element} to save`);
+                    return;
                 }
-                if ((element != "scouterNum") || (element != "initials")) {
-                    input.value = ""
+                if (element != "scouterNum" || element != "initials") {
+                    input.value = "";
                 }
-                break
+                break;
         }
 
-        formData[element] = data
+        formData[element] = data;
     }
 
-    const jsonFile = JSON.stringify(formData, null, 2)
-    jsonFull = jsonFull ? jsonFull + ",\n" + jsonFile : jsonFile
-    localStorage["jsonData"] = jsonFull
+    const jsonFile = JSON.stringify(formData, null, 2);
+    jsonFull = jsonFull ? jsonFull + ",\n" + jsonFile : jsonFile;
+    localStorage["jsonData"] = jsonFull;
 
-    const file = new Blob([jsonFile], { type: 'text/plain' })
+    const file = new Blob([jsonFile], { type: "text/plain" });
 
-    const link = document.createElement("a")
-    link.href = URL.createObjectURL(file)
-    link.download = `M${formData.matchNumber}_T${formData.teamNumber}.json`
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(file);
+    link.download = `M${formData.matchNumber}_T${formData.teamNumber}.json`;
 
-    link.click()
-    URL.revokeObjectURL(link.href)
+    link.click();
+    URL.revokeObjectURL(link.href);
 }
